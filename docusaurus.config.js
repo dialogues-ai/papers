30efdeb99a80d2b50c7d7acfb0e48a07afa167ca
jonnyjohnson1 <jonnyjohnson1@gmail.com>
2022-09-28 14:57:23 -0500

    change edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/dialogues-ai/docs/tree/main/',
+            'https://github.com/dialogues-ai/papers/tree/main/',
         },
         // blog: {
         //   showReadingTime: true,
